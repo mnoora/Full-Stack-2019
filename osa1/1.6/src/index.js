c64a7c3d6642bfb779037e1cdadc67476aff9fde
Noora Mäkelä <noora.m.makela@helsinki.fi>
2019-01-20 18:43:19 +0200

    1.6 valmis

---
+++ osa1/1.6/src/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = (newValue) => {
+    setGood(newValue)
+  }
+  const setToNeutral = (newValue) => {
+    setNeutral(newValue)
+  }
+  const setToBad = (newValue) => {
+    setBad(newValue)
+  }
+
+  return (
+    <div>
+      <h1> anna palautetta</h1>
+      <Button handleClick={()=> setToGood(good + 1)} text="hyvä" />
+      <Button handleClick={()=> setToNeutral(neutral + 1)} text="neutraali" />
+      <Button handleClick={()=> setToBad(bad + 1)} text="huono" />
+      <h1> statistiikka</h1>
+      <p>hyvä {good} </p>
+      <p> neutraali {neutral} </p>
+      <p> huono {bad} </p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
