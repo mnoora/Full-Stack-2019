a4acb25e261f08a2ab352625c0d0d59c6ec143d2
Noora Mäkelä <noora.m.makela@helsinki.fi>
2019-01-28 19:11:50 +0200

    2.6 valmis

---
+++ osa2/2.6/src/components/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+        name: newName,
+        date: new Date().toISOString(),
+        important: Math.random() > 0.5,
+        id: persons.length + 1,
+      }
+    
+      setPersons(persons.concat(personObject))
+      setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          nimi: <input 
+          value= {newName}
+          onChange={handleNameChange} 
+          />
+        </div>
+        <div>
+          <button type="submit">lisää</button>
+        </div>
+      </form>
+      <h2>Numerot</h2>
+      {persons.map(person =>
+        <p key={person.name}> {person.name} </p> )}
+    </div>
+  )
+
+}
+
+export default App
