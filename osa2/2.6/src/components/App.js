32a67d2a7e1b1eafa8d5a2b3cc7ed6ea6e314271
Noora Mäkelä <noora.m.makela@helsinki.fi>
2019-01-28 20:22:47 +0200

    2.9 valmis

--- osa2/2.6/src/components/App.js
+++ osa2/2.6/src/components/App.js
@@ -2,10 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '045-123456' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-123456' },
+    { name: 'Arto Järvinen', number: '040-123456' },
+    { name: 'Lea Kutvonen', number: '040-123456' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
+  const [ findName, setFindName] = useState('')
+
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(findName.toLowerCase()))
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -36,10 +42,16 @@ const App = () => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+  const handleFindNameChange = (event) => {
+    console.log(event.target.value)
+    setFindName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+      rajaa näytettäviä <input value={findName} onChange={handleFindNameChange} />
+      <h2> lisää uusi</h2>
       <form onSubmit={addPerson}>
         <div>
           nimi: <input 
@@ -58,7 +70,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numerot</h2>
-      {persons.map(person =>
+      {personsToShow.map(person =>
         <p key={person.name}> {person.name} {person.number} </p> )}
     </div>
   )
