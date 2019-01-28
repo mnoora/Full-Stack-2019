9814c3996d5123f5b80f6bbc2947f7b848aebccf
Noora Mäkelä <noora.m.makela@helsinki.fi>
2019-01-28 18:19:08 +0200

    2.3 ja 2.4 valmiit

--- osa2/2.1/src/index.js
+++ osa2/2.1/src/index.js
@@ -47,30 +47,52 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-    const course = {
-      name: 'Half Stack -sovelluskehitys',
-      parts: [
-        {
-          name: 'Reactin perusteet',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Tiedonvälitys propseilla',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'Komponenttien tila',
-          exercises: 14,
-          id: 3
-        }
-      ]
-    }
+    const courses = [
+      {
+        name: 'Half Stack -sovelluskehitys',
+        id: 1,
+        parts: [
+          {
+            name: 'Reactin perusteet',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Tiedonvälitys propseilla',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'Komponenttien tila',
+            exercises: 14,
+            id: 3
+          }
+        ]
+      },
+      {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 2,
+            id: 1
+          },
+          {
+            name: 'Middlewaret',
+            exercises: 7,
+            id: 2
+          }
+        ]
+      }
+    ]
   
     return (
       <div>
-        <Course course={course} />
+        <Header course="Opetusohjelma"/>
+        {courses.map( course => (
+            <Course key={course.id} course={course} />
+            ))}
       </div>
     )
   }
