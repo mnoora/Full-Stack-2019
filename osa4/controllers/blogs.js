56855c9106a9468de6cd326feef3efe88b32c276
Noora Mäkelä <noora.m.makela@helsinki.fi>
2019-02-17 18:16:28 +0200

    1.14 asti valmis

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -23,4 +23,43 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
+blogsRouter.get('/:id', async (request, response, next) => {
+  try{
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog.toJSON())
+    } else {
+      response.status(404).end()
+    }
+  } catch(exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  try {
+    const body = request.body
+
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    }
+    const updateBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new:true })
+    response.json(updateBlog.toJSON())
+  } catch(exception) {
+    next(exception)
+  }
+})
+
 module.exports = blogsRouter
