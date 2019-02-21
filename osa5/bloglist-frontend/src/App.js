6a201389f0c8e5f168a1bdb69cdb4fb5bce50d40
Noora Mäkelä <noora.m.makela@helsinki.fi>
2019-02-21 20:00:49 +0200

    5.8 valmis

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -164,7 +164,7 @@ const handleUrlChange = (event) => {
       <br/>
       {blogForm()}
       <br/>
-      {blogs.map(blog =>
+      {blogs.sort((a,b) => b.likes-a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} like={like} />
       )}
       </div>
